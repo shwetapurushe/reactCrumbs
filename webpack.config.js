715c6d71fd069f39e394e2567a822cd3c8547048
shwetapurushe <shwetapurushe@gmail.com>
2016-01-05 16:35:24 -0500

    webpack build : config and bundle.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,25 @@
 /**
  * Created by Shweta on 1/5/2016.
  */
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports ={
+    entry : './main.js',
+    output : {path: __dirname + "/dist", filename:'bundle.js'},
+    module :{
+        loaders :[{
+            test : /\.(js|jsx)$/,
+            exclude : '/node_modules/',
+            loader : 'babel-loader',
+            query : {
+                presets : ['es2015', 'react']
+            }
+        }]
+    },
+    externals : {
+        "react" : "React",
+        "react-dom": "ReactDOM"
+    }
+
+};
